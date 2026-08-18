25b93f8d546f141bc21643355c2a43a4a17f61ae
Tommaso Tralli <trallitommaso2001@gmail.com>
2026-08-18 16:12:45 +0200

    Sincronizzazione play_random.py

--- src/play_random.py
+++ src/play_random.py
@@ -4,7 +4,7 @@ ENV_ID = "CarRacing-v2"  # cambia in v3 se serve
 
 def main():
     env = gym.make(ENV_ID, render_mode="human")
-    obs, info = env.reset()
+    obs, info = env.reset(seed=12)
     while True:
         action = env.action_space.sample()
         obs, reward, terminated, truncated, info = env.step(action)
