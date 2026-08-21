fe253a09d63775caf564c8d5ddf3fab78c460f97
Tommaso Tralli <trallitommaso2001@gmail.com>
2026-08-21 15:05:55 +0200

    Aggiunta evaluation periodica

--- src/train.py
+++ src/train.py
@@ -2,7 +2,10 @@ import argparse
 import os
 
 from stable_baselines3 import PPO, DQN
+from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.logger import configure
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage
 
 from env import make_env
 
@@ -49,6 +52,27 @@ def main():
         required = True,
     )
 
+    # Ogni quanti timesteps effettuare la valutazione del modello
+    parser.add_argument(
+        "--eval-freq",
+        type=int,
+        default=10_000,
+    )
+
+    # Numero di episodi utilizati per ogni valutazione periodica
+    parser.add_argument(
+        "--eval-episodes",
+        type=int,
+        default=5,
+    )
+
+    # Seed usato per inizializzare l'ambiente di evaluation
+    parser.add_argument(
+        "--eval-seed",
+        type=int,
+        default=100
+    )
+
     args = parser.parse_args()
 
     # Creo un'etichetta compatta per il numero di timesteps e costruisco un identificatore univoco del training
@@ -57,10 +81,20 @@ def main():
 
     env = make_env()
 
+    # Creo un ambiente separato solo per valutare il modello
+    eval_env = DummyVecEnv([
+        lambda: Monitor(make_env())
+    ])
+
+    eval_env = VecTransposeImage(eval_env)
+
+    
+    eval_env.seed(args.eval_seed)
+
     os.makedirs("models", exist_ok=True)
 
-    # Cartella in cui vengno salvati i dati di training
-    log_dir = os.path.join("logs", run_name);
+    # Cartella in cui vengono salvati i log del training e delle evaluation.
+    log_dir = os.path.join("logs", run_name)
 
     # Mostra i log nel terminale e li salva anche in formato CSV
     logger = configure(log_dir, ["stdout","csv"])
@@ -86,18 +120,32 @@ def main():
     )
 
     # Uso del logger per registrare il training
-    model.set_logger(logger)    
+    model.set_logger(logger)
 
-    model.learn(total_timesteps=args.timesteps)
+    # Valuta periodicamente il modello durante il training
+    eval_callback = EvalCallback(
+        eval_env,                               # Ambiente in cui viene effettuata la valutazione
+        n_eval_episodes=args.eval_episodes,     # Numero di episodi eseguiti per ogni evaluation
+        eval_freq=args.eval_freq,               # Ogni quanti step la valutazione viene effettuata
+        log_path=log_dir,                       # Cartella in cui vengono salvati i risultati delle evaluation
+        deterministic=True,                     # La valutazione dell'agente avviene senza introdurre esplorazione casuale
+        render=False,                           # Non viene aperta alcuna finestra grafica
+        verbose=1,                              
+    )       
 
-    timesteps_label = format_timesteps(args.timesteps)
+    # Avvia il training ed esegue periodicamente l'evaluation.
+    model.learn(
+        total_timesteps=args.timesteps,
+        callback=eval_callback,
+    )
 
+    # Salva il modello al termine del training.
     model.save(
-    f"models/{args.algo}_{args.run_type}_{timesteps_label}_seed_{args.seed}"
+        f"models/{run_name}"
     )
 
     env.close()
-
+    eval_env.close()
 
 if __name__ == "__main__":
     main()
