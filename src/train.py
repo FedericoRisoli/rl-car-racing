7d9b4ac97aa27a3827dd2765c6fcd9a99eb356b9
Tommaso Tralli <trallitommaso2001@gmail.com>
2026-08-21 12:24:15 +0200

    Aggiunta logging SB3

--- src/train.py
+++ src/train.py
@@ -2,10 +2,11 @@ import argparse
 import os
 
 from stable_baselines3 import PPO, DQN
+from stable_baselines3.common.logger import configure
 
 from env import make_env
 
-# Funzione utilizzata per attribuire nomi standard ai modelli
+# Funzione che converte il numero di timesteps in un'etichetta compatta
 def format_timesteps(timesteps: int) -> str:
     if timesteps % 1_000_000 == 0:
         return f"{timesteps // 1_000_000}M"
@@ -17,32 +18,52 @@ def format_timesteps(timesteps: int) -> str:
 
 def main():
     parser = argparse.ArgumentParser()
+
+    # Parametri
+
+    # Algoritmo di RL da utilizzare
     parser.add_argument(
         "--algo",
         choices=["ppo", "dqn"],
         required=True,
     )
+
+    # Numero di environment steps richiesti per il training
     parser.add_argument(
         "--timesteps",
         type=int,
         default=2000,
     )
+
+    # Seed utilizzato per rendere il training riproducibile
     parser.add_argument(
         "--seed",
         type=int,
         default=0,
     )
+
+    # Tipo di training
     parser.add_argument(
         "--run-type",
         choices=["smoke", "pilot", "final"],
-        default="smoke",
+        required = True,
     )
 
     args = parser.parse_args()
 
+    # Creo un'etichetta compatta per il numero di timesteps e costruisco un identificatore univoco del training
+    timesteps_label = format_timesteps(args.timesteps)
+    run_name = (f"{args.algo}_{args.run_type}_{timesteps_label}_seed_{args.seed}")
+
     env = make_env()
 
     os.makedirs("models", exist_ok=True)
+
+    # Cartella in cui vengno salvati i dati di training
+    log_dir = os.path.join("logs", run_name);
+
+    # Mostra i log nel terminale e li salva anche in formato CSV
+    logger = configure(log_dir, ["stdout","csv"])
 
     if args.algo == "ppo":
         model = PPO(
@@ -62,8 +83,10 @@ def main():
     verbose=1,
     buffer_size=10_000,
     learning_starts=500,
-)
-        
+    )
+
+    # Uso del logger per registrare il training
+    model.set_logger(logger)    
 
     model.learn(total_timesteps=args.timesteps)
 
